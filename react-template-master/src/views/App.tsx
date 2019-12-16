4ca3ba2df39c4d55a1fc43426120a2e35cda361e
hamono <487496627@qq.com>
2019-12-16 17:46:11 +0800

    add right

--- react-template-master/src/views/App.tsx
+++ react-template-master/src/views/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Header from './Header';
 import Body from "./Body";
+import "./base.scss";
 
 
 export default function App() {
--- react-template-master/src/views/Header/Top/Right/index.tsx
+++ react-template-master/src/views/Header/Top/Right/index.tsx
@@ -2,5 +2,14 @@ import * as React from "react";
 import * as style from "./style.scss"
 
 export default function Right(){
-    return <div className={style.a}>12</div>
+    return <div className={style.a}>
+        <a href="#" className={style.b}>图片</a>
+        <a href="#" className={style.b}>消息</a>
+        <a href="#" className={style.b}>动态</a>
+        <a href="#" className={style.b}>收藏</a>
+        <a href="#" className={style.b}>历史</a>
+        <a href="#" className={style.b}>大会员</a>
+        <a href="#" className={style.b}>创作中心</a>
+        <a href="#" className={style.b}>投稿</a>
+    </div>
 }
