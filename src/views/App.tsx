7e9b2a557dfa52440e26dc84cbb6cdeebcc7b59c
吴聿轩 <929003281@qq.com>
2020-01-18 12:03:34 +0800

    修改文件夹结构

---
+++ src/views/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Header from './Header';
+import Body from "./Body";
+import "./base.scss";
+
+
+export default function App() {
+    return <div >
+        <Header />
+        <Body />
+    </div>;
+}
