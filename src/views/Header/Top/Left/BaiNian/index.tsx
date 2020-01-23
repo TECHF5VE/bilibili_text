b82acd4e7a7a4f3c0c4753949baab7e8f91f7fe4
hamono <487496627@qq.com>
2020-01-23 12:55:54 +0800

    添加拜年祭图片

--- src/views/Header/Top/Left/BaiNian/index.tsx
+++ src/views/Header/Top/Left/BaiNian/index.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import * as style from "./style.scss";
+import img from "./img1.png";
 
 export default function BaiNian() {
   return (
   <div className={style.box}>
     <div className={style.minBox}>
       <a href="#" className={style.moveCentext}>拜年祭</a>
-      <a href="#" className={style.moveCentext}><img src="img-1.png" className={style.move}/></a>
+      <a href="#" className={style.moveCentext}><img src={img} className={style.move}/></a>
       <a href="#" className={style.moveCentext}>拜年祭</a>
     </div>
   </div>
