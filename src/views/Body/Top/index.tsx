da11abea9c81b4b009933c68921532100b86691d
hamono <487496627@qq.com>
2020-01-26 19:09:10 +0800

    body.top

--- src/views/Body/Top/index.tsx
+++ src/views/Body/Top/index.tsx
@@ -10,31 +10,31 @@ export default function Top() {
     <div className={style.left}>
       <Carousel autoplay={true} dots={true} dotPosition="bottom">
         <div className={style.picture1}>
-          <a href="#">
+          <a href="">
             <p>这是一个p元素，给它设置了绝对定位</p>
             {/* <img src={img} className={style.icon1} /> */}
           </a>
         </div>
         <div className={style.picture2}>
-          <a href="#">
+          <a href="">
             <p>让它做这张图片的description部分</p>
             {/* <img src={img} className={style.icon1} /> */}
           </a>
         </div>
         <div className={style.picture3}>
-          <a href="#">
+          <a href="">
             <p>给它设置top，让它下来一点</p>
             {/* <img src={img} className={style.icon1} /> */}
           </a>
         </div>
         <div className={style.picture4}>
-          <a href="#">
+          <a href="">
             <p>再给他设置left，它却没了</p>
             {/* <img src={img} className={style.icon1} /> */}
           </a>
         </div>
         <div className={style.picture5}>
-          <a href="#">
+          <a href="">
             <p>就设置了margin-left，他又可以了</p>
             {/* <img src={img} className={style.icon1} /> */}
           </a>
@@ -44,40 +44,46 @@ export default function Top() {
     <div className={style.right}>
       <div className={style.line1}>
        
-        <div className={style.action} onMouseMove={()=>{
-          const move=document.querySelector('.h4');
-          move.style.backgroundColor='rgba(0,0,0,0.5)';
-          move.style.marginTop='0px';
-        }}>
-          <a href="#" className={style.hidden}>
+        <div className={style.action} >
+          <a href="" >
           <Upwait />
           <img src={img} className={style.icon2} />
         </a>
       </div>
       <div className={style.action}>
-        <a href="#">
+        <a href="">
           <img src={img} className={style.icon2} />
         </a>
       </div>
       <div className={style.action}>
-        <a href="#">
+        <a href="">
+          <img src={img} className={style.icon2} />
+        </a>
+      </div>
+      <div className={style.action}>
+        <a href="">
           <img src={img} className={style.icon2} />
         </a>
       </div>
     </div>
     <div className={style.line1}>
       <div className={style.action}>
-        <a href="#">
+        <a href="">
           <img src={img} className={style.icon2} />
         </a>
       </div>
       <div className={style.action}>
-        <a href="#">
+        <a href="">
           <img src={img} className={style.icon2} />
         </a>
       </div>
       <div className={style.action}>
-        <a href="#">
+        <a href="">
+          <img src={img} className={style.icon2} />
+        </a>
+      </div>
+      <div className={style.action}>
+        <a href="">
           <img src={img} className={style.icon2} />
         </a>
       </div>
