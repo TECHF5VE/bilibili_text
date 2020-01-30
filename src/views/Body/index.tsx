b341677f8e269e3253e472c0f61e9070c46b2f5b
hamono <487496627@qq.com>
2020-01-30 14:17:22 +0800

    删除无用文件

--- src/views/Body/index.tsx
+++ src/views/Body/index.tsx
@@ -1,27 +1,19 @@
 import * as React from "react";
 import * as style from "./style.scss";
-<<<<<<< HEAD
-import DianShiJu from "./DianShiJu";
 import TuiGuang from "./TuiGuang";
 import BaiNianJi from "./BaiNianJi";
-import Top from "./Top"
-=======
 import First from "./First";
 import Top from "./Top";
 import Second from "./Second";
->>>>>>> 1578e0b41d9a951fbb51d4a48186315561be698f
 
 export default function Body() {
   return <div className={style.a}>
     <Top />
-<<<<<<< HEAD
   <TuiGuang />
   <BaiNianJi />
-    <DianShiJu />
+  
  
-=======
     <First />
     <Second />
->>>>>>> 1578e0b41d9a951fbb51d4a48186315561be698f
   </div>
 }
