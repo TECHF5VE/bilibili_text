f46224559968f9467d353904e63030aed36d6aa8
hamono <487496627@qq.com>
2020-02-01 01:46:24 +0800

    首页完成

---
+++ src/views/Picture/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Affix } from "antd";
+import * as style from "./style.scss";
+import img from "./Xiao.png";
+
+export default function Picture() {
+
+  return <div className={style.box}>
+
+    <Affix offsetTop={600}>
+      <img src={img}/>
+    </Affix>
+  </div>
+}
