6f41cf645acb79085b9fad59298195fad6ce8e66
hamono <487496627@qq.com>
2020-02-13 23:24:34 +0800

    一键清空

--- src/views/Body/Fourth/Left/Body/Upcontext/Component/TodoList/Top/index.tsx
+++ src/views/Body/Fourth/Left/Body/Upcontext/Component/TodoList/Top/index.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import * as style from "./style.scss";
 import { Button } from "antd";
 import img from "./img.png";
+import { CounterContext } from "../../CounterContext";
 
 export default function Top(props:{number:number}) {
+  const {setList } = React.useContext(CounterContext);
+  const removeAll=React.useCallback(()=>{setList([])},[])
   return <div className={style.box}>
     <div className={style.left}>
       <img src={img} width="30px" height="30px"/><span>稍后再看 ({props.number}/100)</span>
     </div>
     <div className={style.right}>
       <Button type="primary" className={style.button} >全部播放</Button>
-      <Button type="primary" className={style.button}>一键清空</Button>
+      <Button type="primary" className={style.button} onClick={removeAll}>一键清空</Button>
       <Button type="primary" className={style.button}>移除已观看视频</Button>
     </div>
   </div>
