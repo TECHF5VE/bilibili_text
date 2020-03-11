f07589364e5cceb645dec83e63d54f457f9f05a0
hamono <487496627@qq.com>
2020-03-11 20:18:20 +0800

    删除无用组件

--- src/views/Body/Animation/Left/Body/Upcontext/Component/TodoList/Top/index.tsx
+++ src/views/Body/Animation/Left/Body/Upcontext/Component/TodoList/Top/index.tsx
@@ -8,7 +8,7 @@ import { Input } from 'antd';
 const { Search } = Input;
 export default function Top(props: { num: number }) {
   const { list, setList } = React.useContext(CounterContext);
-  const removeAll = React.useCallback(() => { setList([]) },[setList])
+  const removeAll = React.useCallback(() => { setList([]) }, [setList])
   const [temp, setTemp] = React.useState<string[]>([]);
   const [inputValue, setInputValue] = React.useState('');
   const handleValue = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
@@ -17,18 +17,15 @@ export default function Top(props: { num: number }) {
 
   // 添加待选项（暂无用）
   const addOptions = React.useCallback(() => {
- 
+    setTemp([]);
     list.map((item: string) => {
-      // console.log(item)
-      // console.log(inputValue==item)
-      // console.log(inputValue)
       if (item.includes(inputValue)) {
-        
-        temp.push(item)
+        setTemp((state) => [...state, item])
+        console.log(temp)
       }
     });
-    // console.log(temp)
-  }, [inputValue,list,temp])
+  }, [inputValue, list, temp])
+
   function change(e: React.ChangeEvent<HTMLInputElement>) {
     handleValue(e)
     addOptions()
@@ -36,16 +33,18 @@ export default function Top(props: { num: number }) {
 
   // 返回搜索结果
   const handleResult = React.useCallback(() => {
-    setTemp(list.filter(v => v.includes(inputValue)));
-  }, [inputValue, list]);
-   return <div className={style.box}>
+    console.log(temp)
+    setList(temp);
+  },[setList, temp]);
+  
+return <div className={style.box}>
     <div className={style.left}>
       <img className={style.img} src={img} width="30px" height="30px" /><span>稍后再看 ({props.num}/100)</span>
     </div>
     <div>
       <Search placeholder="input search text" value={inputValue} enterButton={true} style={{ width: 300 }}
         onChange={change} onSearch={handleResult} />
-      {/* <ul className={style.ul}>{temp.map((Item: string, i: number) => <li className={style.li} key={i} >{Item}</li>)}</ul> */}
+      <ul className={style.ul}>{temp.map((Item: string, i: number) => <li className={style.li} key={i} >{Item}</li>)}</ul>
     </div>
     <div className={style.right}>
       <Button type="primary" className={style.button} >全部播放</Button>
