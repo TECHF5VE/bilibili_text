82f3ed1a2c9ccbb80818f9ea54b85e7481e09b00
hamono <hamono@users.noreply.github.com>
2020-03-14 12:07:21 +0000

    添加json schema 20c639ac94690e259f762432bd5c4c375f844864

--- src/useFetch/api/index.d.ts
+++ src/useFetch/api/index.d.ts
@@ -1,7 +1,21 @@
-import { ExampleUserPostRequest } from './user/post/request';
-import { ExampleUserPostResponse } from './user/post/response';
+import { ApiUserLoginRequest } from './api/user/login/request';
+import { ApiUserLoginResponse } from './api/user/login/response';
+import { ApiUserLogonRequest } from './api/user/logon/request';
+import { ApiUserLogonResponse } from './api/user/logon/response';
+import { ExampleUserPostRequest } from './example/user/post/request';
+import { ExampleUserPostResponse } from './example/user/post/response';
 export default interface Api {
-  'user': {
+  'api/user': {
+    login: {
+      request: ApiUserLoginRequest;
+      response: ApiUserLoginResponse;
+    };
+    logon: {
+      request: ApiUserLogonRequest;
+      response: ApiUserLogonResponse;
+    };
+  };
+  'example/user': {
     post: {
       request: ExampleUserPostRequest;
       response: ExampleUserPostResponse;
