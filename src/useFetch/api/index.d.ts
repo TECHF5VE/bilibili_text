1ae2e1d37112851ae75aacac4c9e321ff18d9d6d
hamono <hamono@users.noreply.github.com>
2020-03-15 08:22:28 +0000

    修改schema 528da2b67833363bab7eb5ff4e7d4cb093d9c476

--- src/useFetch/api/index.d.ts
+++ src/useFetch/api/index.d.ts
@@ -1,24 +1,26 @@
-import { ApiUserLoginRequest } from './api/user/login/request';
-import { ApiUserLoginResponse } from './api/user/login/response';
-import { ApiUserLogonRequest } from './api/user/logon/request';
-import { ApiUserLogonResponse } from './api/user/logon/response';
 import { ExampleUserPostRequest } from './example/user/post/request';
 import { ExampleUserPostResponse } from './example/user/post/response';
+import { UserLoginPostRequest } from './user/login/post/request';
+import { UserLoginPostResponse } from './user/login/post/response';
+import { UserSignupPostRequest } from './user/signup/post/request';
+import { UserSignupPostResponse } from './user/signup/post/response';
 export default interface Api {
-  'api/user': {
-    login: {
-      request: ApiUserLoginRequest;
-      response: ApiUserLoginResponse;
-    };
-    logon: {
-      request: ApiUserLogonRequest;
-      response: ApiUserLogonResponse;
-    };
-  };
   'example/user': {
     post: {
       request: ExampleUserPostRequest;
       response: ExampleUserPostResponse;
+    };
+  };
+  'user/login': {
+    post: {
+      request: UserLoginPostRequest;
+      response: UserLoginPostResponse;
+    };
+  };
+  'user/signup': {
+    post: {
+      request: UserSignupPostRequest;
+      response: UserSignupPostResponse;
     };
   };
 }
