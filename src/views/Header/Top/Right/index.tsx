e8caf5e6b85d87cb204eab64a9494211b4d36af7
syntou <kumaizi23@163.com>
2020-05-01 14:34:35 +0800

    add 登录

--- src/views/Header/Top/Right/index.tsx
+++ src/views/Header/Top/Right/index.tsx
@@ -3,16 +3,17 @@ import * as style from './style.scss';
 import Upload from './Upload';
 import OpenGitHub from './OpenGitHub';
 import { Link } from 'web-router';
-import { Avatar,Popover } from 'antd';
+import { Avatar } from 'antd';
+//import {Popover} from 'antd';
 export default function Right() {
-  const {Success,setSuccess}=React.useContext()
-  const xiaLa=(
-    <div>
-      <div>个人中心</div>
-      <Link to="" onClick={()=>setSuccess(false)}><div>退出登录</div></Link>
-    </div>
-  )
-  if (Success) {
+  // const {Success,setSuccess}=React.useContext()
+  // const xiaLa=(
+  //   <div>
+  //     <div>个人中心</div>
+  //     <Link to="" onClick={()=>setSuccess(false)}><div>退出登录</div></Link>
+  //   </div>
+  // )
+  //if (Success) {
     return (
       <div className={style.a}>
         <div className={style.signIn}>
@@ -36,26 +37,27 @@ export default function Right() {
         <OpenGitHub />
       </div>
     );
-  } else {
-    return (
-      <div className={style.a}>
-        <div className={style.signIn}>
-        <span className={style.span}>syntou</span>
-        <button><Popover content={xiaLa}>
-          <Avatar size="large" icon="user" />
-         </Popover>
-         </button>
-        </div>
+ // }
+  //  else {
+  //   return (
+  //     <div className={style.a}>
+  //       <div className={style.signIn}>
+  //       <span className={style.span}>syntou</span>
+  //       <button><Popover content={xiaLa}>
+  //         <Avatar size="large" icon="user" />
+  //        </Popover>
+  //        </button>
+  //       </div>
 
-        <Link to="/whachLater/" target="_blank" className={style.b}>
-          稍后再看
-        </Link>
-        <a className={style.b}>收藏</a>
-        <a className={style.b}>大会员</a>
-        <a className={style.b}>创作中心</a>
-        <Upload />
-        <OpenGitHub />
-      </div>
-    );
-  }
+  //       <Link to="/whachLater/" target="_blank" className={style.b}>
+  //         稍后再看
+  //       </Link>
+  //       <a className={style.b}>收藏</a>
+  //       <a className={style.b}>大会员</a>
+  //       <a className={style.b}>创作中心</a>
+  //       <Upload />
+  //       <OpenGitHub />
+  //     </div>
+  //   );
+  // }
 }
